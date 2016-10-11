2e6cff22712636209f9ad71a57185db242b318b4
Andrew <Andrew@tests-MacBook-Pro.local>
2016-10-11 14:26:48 -0400

    add incanter deps and new guesser func

--- die/project.clj
+++ die/project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/math.combinatorics "0.1.3"]]
+                 [org.clojure/math.combinatorics "0.1.3"]
+                 [incanter "1.2.3-SNAPSHOT"]]
   :main ^:skip-aot die.core
-  :plugins [[lein-gorilla "0.3.6"]]
 
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
