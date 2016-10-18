ff1a3077530bdf776fd02314f8471c49eaf6d9eb
Andrew <Andrew@tests-MacBook-Pro.local>
2016-10-18 12:00:35 -0400

    added a generalized version for some of the die and coins problems

---
+++ Coin Problems/coins/src/coins/core.clj
@@ -0,0 +1,44 @@
+(ns coins.core
+  (:use clojure.set)
+  (:require 
+    (incanter [core :refer :all]
+              [stats :refer :all])))
+
+(defn roll-die [die-faces] (sample (range 1 (inc die-faces)) :size 1))
+
+(defn roll-and-toss [d1 num-outcomes consec]
+ (fn [roll-outcome] 
+   (let [
+        ways-of-winning (if (not= consec "c") (choose roll-outcome num-outcomes) 1)
+        prob-coin-toss (/ 1 2)
+        prob-outcomes-true (Math/pow prob-coin-toss num-outcomes)
+        prob-rest-not-true (Math/pow prob-coin-toss (- roll-outcome num-outcomes))]
+    
+        (if (< roll-outcome num-outcomes)
+          0
+          (*
+            ways-of-winning
+            prob-outcomes-true
+            prob-rest-not-true)))))
+
+
+(defn generalized [d1 num-outcomes consec]
+  (let [potential-outcomes (range 1 (inc d1))
+        outcome-likelihood (roll-and-toss d1 num-outcomes consec)
+        likelihood-list (map outcome-likelihood potential-outcomes)]
+    likelihood-list))
+
+
+
+(defn problem-f []
+  (let [likelihood-list (generalized 6 3 "nc")
+        adjusted-likelihoods (map #(* (/ 1 6) %) likelihood-list) ]
+    (reduce + adjusted-likelihoods)))
+
+
+
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ Coin Problems/coins/test/coins/core_test.clj
@@ -0,0 +1,7 @@
+(ns coins.core-test
+  (:require [clojure.test :refer :all]
+            [coins.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
